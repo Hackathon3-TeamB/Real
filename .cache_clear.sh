39de2e680573f20f75f743e1e7b1cbcc9bf1d7b9
Junichi Nishi <github@purewest.dip.jp>
2021-10-18 20:42:08 +0900

    Dockerコンテナを初期構成

---
+++ .cache_clear.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm -r Django_Project/config/__pycache__
+rm -r Django_Project/login/__pycache__
+rm -r Django_Project/real/__pycache__
+rm -r Django_Project/login/migrations
+rm -r Django_Project/real/migrations
---
+++ .migration.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+docker-compose exec django python3 manage.py makemigrations login
+docker-compose exec django python3 manage.py makemigrations real
+docker-compose exec django python3 manage.py migrate auth
+docker-compose exec django python3 manage.py migrate --noinput
+docker-compose exec django python3 manage.py collectstatic --no-input
